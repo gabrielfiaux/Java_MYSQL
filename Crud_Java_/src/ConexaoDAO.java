308bf4d09d2afde10d3876960dcf4c37638614f8
gabrielfiaux <gfiauxreis@gmail.com>
2024-10-18 20:58:50 -0300

    Primeiro Commit
    Criada a classe ConexaoDAO.
    Adicionado o Driver de conexão a biblioteca.

---
+++ Crud_Java_/src/ConexaoDAO.java
@@ -0,0 +1,25 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConexaoDAO {
+
+    public Connection conectaBD() {
+        Connection conn = null;
+
+        //se houver um erro em um pc diferente pode estar aqui
+        // pois o user está como localhost
+        //banco de dados está como banco teste.
+        try {
+            String url = "jdbc:mysql://localhost3306/bancoteste?user=root&password=";
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ConexaoDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+
+}
